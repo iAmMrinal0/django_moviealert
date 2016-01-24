db8436eddf7617fee4a57185916fcbb3e38a58af
Mrinal Purohit <mrinal1892005@gmail.com>
2016-01-24 23:11:03 +0530

    Change task to periodic task which runs every hour

--- moviealert/tasks.py
+++ moviealert/tasks.py
@@ -1,4 +1,5 @@
-from celery.decorators import task
+from celery.decorators import periodic_task
+from celery.schedules import crontab
 from celery.utils.log import get_task_logger
 
 from moviealert.search import search_movie
@@ -6,7 +7,7 @@ from moviealert.search import search_movie
 logger = get_task_logger(__name__)
 
 
-@task(name="search_movie_task")
+@periodic_task(run_every=(crontab(hour="*", minute="0", day_of_week="*")))
 def search_movie_task():
     """search a movie and print if found successfully"""
     logger.info("Finding a movie")
