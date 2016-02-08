c24dc7db961b03c947a98454fc3e8655c5f938ff
Mrinal Purohit <mrinal1892005@gmail.com>
2016-02-08 22:20:15 +0530

    Fix css and heading test also removed localization test as no longer required

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -21,21 +21,14 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.browser.get(self.get_full_url("home"))
         self.assertIn("Alert", self.browser.title)
 
-    def test_h1_css(self):
+    def test_h2_css(self):
         self.browser.get(self.get_full_url("home"))
-        h1 = self.browser.find_element_by_tag_name("h1")
-        self.assertIn(h1.value_of_css_property(
-            "color"), "rgba(200, 50, 255, 1)")
+        h2 = self.browser.find_element_by_tag_name("h2")
+        self.assertIn(h2.value_of_css_property(
+            "color"), "rgba(0, 0, 0, 1)")
 
     def test_home_files(self):
         self.browser.get(self.live_server_url + "/robots.txt")
         self.assertNotIn("Not Found", self.browser.title)
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
-
-    def test_localization(self):
-        today = date.today()
-        self.browser.get(self.get_full_url("home"))
-        local_date = self.browser.find_element_by_id("local-date")
-        self.assertEqual(formats.date_format(
-            today, use_l10n=True), local_date.text)
